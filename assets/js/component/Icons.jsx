30146b471b0588fbc461ec1b3f4bfa741a42a54c
takahiro-saeki <hiroooon69@gmail.com>
2016-08-16 10:59:07 +0900

    commit!

---
+++ assets/js/component/Icons.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+export default class Icons extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    icon: '',
+    size: 'fa-lg',
+    aria: 'true',
+    styles: {}
+  }
+
+  render() {
+    return (
+      <i className={classNames('fa', this.props.icon, this.props.size)}
+         aria-hidden={this.props.aria} style={Object.assign({}, this.props.styles)}/>
+    )
+  }
+}
--- assets/js/component/Social.jsx
+++ assets/js/component/Social.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import Icons from './icons';
 import style from '../../css/style';
 
 export default class Social extends Component {
   render() {
     return (
-      <section>ソーシャルアカウントの紹介</section>
+      <section>
+        <Icons
+          icon="fa-twitter"
+          size="fa-5x"/>
+      </section>
     )
   }
 }

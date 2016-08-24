de3f99f7858d22f1e68cdcf10cdbefca2ef5ec1d
takahiro-saeki <hiroooon69@gmail.com>
2016-08-24 15:49:14 +0900

    add resume basic style.

--- assets/js/component/Resume.jsx
+++ assets/js/component/Resume.jsx
@@ -1,10 +1,72 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
 import style from '../../css/style';
 
 export default class Resume extends Component {
   render() {
+    const boxLeft = classNames(style['col-xs-12'], style['col-md-4'], style['resume-margin']);
+    const boxRight = classNames(style['col-xs-12'], style['col-md-8'], style['resume-margin']);
+    const first = classNames(style['resume-box'], style.first);
+    const description =classNames(style['resume-box'], style['resume-desc'])
     return (
-      <div>timeline的な実績</div>
+      <div className={style['container-fluid']}>
+        <section className={style.row}>
+          <div className={boxLeft}>
+            <div className={first}>
+              <h2>mobile game company</h2>
+              <h3>2016/5 ~ 2016/6</h3>
+            </div>
+          </div>
+          <div className={boxRight}>
+            <div className={style['resume-box']}>
+              I implemented the function of social matching service application.<br />
+              I used ECMAScript 2015, gulp, React.js, PostCSS, and some modern library.
+            </div>
+          </div>
+        </section>
+        <section className={style.row}>
+          <div className={boxLeft}>
+            <div className={first}>
+              <h2>mobile game company</h2>
+              <h3>2016/5 ~ 2016/6</h3>
+            </div>
+          </div>
+          <div className={boxRight}>
+            <div className={style['resume-box']}>
+              I implemented the function of social matching service application.<br />
+              I used ECMAScript 2015, gulp, React.js, PostCSS, and some modern library.
+            </div>
+          </div>
+        </section>
+        <section className={style.row}>
+          <div className={boxLeft}>
+            <div className={first}>
+              <h2>mobile game company</h2>
+              <h3>2016/5 ~ 2016/6</h3>
+            </div>
+          </div>
+          <div className={boxRight}>
+            <div className={style['resume-box']}>
+              I implemented the function of social matching service application.<br />
+              I used ECMAScript 2015, gulp, React.js, PostCSS, and some modern library.
+            </div>
+          </div>
+        </section>
+        <section className={style.row}>
+          <div className={boxLeft}>
+            <div className={first}>
+              <h2>mobile game company</h2>
+              <h3>2016/5 ~ 2016/6</h3>
+            </div>
+          </div>
+          <div className={boxRight}>
+            <div className={style['resume-box']}>
+              I implemented the function of social matching service application.<br />
+              I used ECMAScript 2015, gulp, React.js, PostCSS, and some modern library.
+            </div>
+          </div>
+        </section>
+      </div>
     )
   }
 }

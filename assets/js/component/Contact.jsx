eb49140e6462d6c4717743f7d7b5ce06bea0906a
takahiro-saeki <hiroooon69@gmail.com>
2016-09-02 05:15:06 -0700

    add image. add input elem.

--- assets/js/component/Contact.jsx
+++ assets/js/component/Contact.jsx
@@ -6,9 +6,13 @@ export default class Header extends Component {
   render() {
     return (
       <section className={style['container-fluid']}>
+        <h2>CONTACT</h2>
         <form className={style.row}>
           <label for="name">name</label>
           <input type="text" id="name" />
+          <label for="mail">name</label>
+          <input type="email" id="mail" />
+          <textarea name="kanso" />
         </form>
       </section>
     )
